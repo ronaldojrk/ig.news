166067ffe0162277b434b2fac409a8ac9c0b664c
Ronaldo jose <ronaldo.jrk@gmail.com>
2021-03-23 17:50:29 -0300

    First Commit 👍

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import Styles from './home.module.scss';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={Styles.contentContainer}>
+        <section className={Styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+
+
+      </main>
+    </>
+  )
+}
