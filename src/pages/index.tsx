6e1e8900a7228ac4296b52c12f6ab6ce0a3604ba
Ronaldo jose <ronaldo.jrk@gmail.com>
2021-03-24 13:41:16 -0300

    adicionando Next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,35 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import Styles from './home.module.scss';
-export default function Home() {
+
+
+//cliente -side 
+
+// server-side 
+//static site generetion
+
+
+// post do blog 
+// conteudo (ssg)
+//comentarios(cliente-side)
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+
+}
+export default function Home({ product }: HomeProps) {
+
+
+
+
+
+
   return (
     <>
       <Head>
@@ -16,10 +43,10 @@ export default function Home() {
 
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
 
         </section>
 
@@ -29,4 +56,27 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IYWDkJX1otHhTsrPdBbMZL9')
+
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+
+    },
+    revalidate: 60 * 60 * 24//24 hours
+  }
+
 }
