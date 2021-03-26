de36d76fe6151876a9b5aa5107e255881c11d3c8
Ronaldo jose <ronaldo.jrk@gmail.com>
2021-03-25 22:15:43 -0300

    webhook no stripe (incompleto)🎉

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,10 @@
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+) {
+  // busca o usuario no banco do faunadb
+  console.log(subscriptionId, customerId);
+  console.log("ronaldo aqui");
+
+  // salvar os dados  da subscription no faundb
+}
